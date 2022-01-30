52bcfcc525866043b13a29ec53ded6193b31f9ef
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-01-30 17:36:04 -0300

    create classe Hero and Wizard

---
+++ Projeto_dotnet_POO/src/Entities/Hero.cs
@@ -0,0 +1,35 @@
+﻿namespace Projeto_dotnet_POO.src.Entities
+{
+
+
+
+    public class Hero
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string Herotype { get; set; }
+
+
+        public Hero(string name, int level, string herotype)
+        {
+            Name = name;
+            Level = level;
+            Herotype = herotype;
+        }
+
+
+        public virtual string Attack()
+        {
+            return Name + " Atacou com sua espada";    
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"{Name}  {Level}  {Herotype}";
+        }
+
+
+    }
+}
---
+++ Projeto_dotnet_POO/src/Entities/Wizard.cs
@@ -0,0 +1,22 @@
+﻿namespace Projeto_dotnet_POO.src.Entities
+{
+    internal class Wizard : Hero
+    {
+        public Wizard(string name, int level, string herotype) : base(name, level, herotype)
+        {
+        }
+
+
+        public override string Attack()
+        {
+            return this.Name + "Lançou Magia";
+        }
+
+        public string Attack(int Bonus)
+        {
+            return this.Name + "Lançou Magia co bonus de ataque"+ Bonus;
+        }
+
+
+    }
+}
